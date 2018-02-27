e1031cee73576c16b1918f462744817528942b1d
Pratik Jagtap <30865275+prat118@users.noreply.github.com>
2018-02-27 17:24:43 +0530

    Replace API key

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -17,7 +17,7 @@ export class ServiceProvider {
   }
 
   getMovie(movieName){
-    var url = 'http://api.themoviedb.org/3/search/movie?query=&query=' + encodeURI(movieName) + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
+    var url = 'http://api.themoviedb.org/3/search/movie?query=&query=' + encodeURI(movieName) + '&api_key=***';
         var response = this.http.get(url).map(res => res.json());
         return response;
   }
