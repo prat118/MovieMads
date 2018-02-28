c406df39e2874d19ce075d2ced8ccb99cc945742
Pratik Jagtap <30865275+prat118@users.noreply.github.com>
2018-02-28 11:11:49 +0530

    Added refresh screen functionality
    Manual refresh function. If you want automatic refresh, only include calls of initRefresh() and stopRefresh() functions.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angu
 import { ServiceProvider } from "../../providers/service/service";
 import { Network } from "@ionic-native/network";
 import { PopoverController } from "ionic-angular";
+import * as moment from 'moment';
 
 import { InfoPage } from "../../pages/info/info";
 import { PopMorePage } from "../../pages/pop-more/pop-more";
@@ -10,89 +11,112 @@ import { AboutusPage } from '../aboutus/aboutus';
 import { ContactUsPage } from '../contact-us/contact-us';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  movies: Array<any>;
+	movies: Array<any>;
+	private timeoutId: number;
+	constructor(public navCtrl: NavController, public navParams: NavParams, private movieService: ServiceProvider, 
+	private toast: ToastController, private network: Network, private popCtrl: PopoverController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private movieService: ServiceProvider, 
-  private toast: ToastController, private network: Network, private popCtrl: PopoverController) {
+	}
+	
+	private initRefresh() {
+		this.refresh();
+		this.timeoutId = setInterval(() => this.refresh(), 5 * 1000);
+	  }
+	  private stopRefresh() {
+		clearInterval(this.timeoutId);
+	  }
 
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
-
-    this.network.onConnect().subscribe(data => {
-      console.log(data)
-      this.displayNetUpdate(data.type);
-    }, err => console.error(err));
-
-    this.network.onDisconnect().subscribe(data => {
-      console.log(data)
-      this.displayNetUpdate(data.type);
-    }, err => console.error(err));
-
-    this.getAllMovies();
-  }
+	  private refresh() {
+		console.log(`Refresh at ${moment().format('LTS')}`);
+	  }
 
 
-  displayNetUpdate(connectStatus: string){
-    let networkType = this.network.type;
-    this.toast.create({
-      message : `You are now ${connectStatus} via ${networkType}`,
-      duration: 3000
-    }).present();
-  }
+	  manualRefresh() {
+		
+		this.initRefresh();
+		var component = this.navCtrl.getActive().instance;
+		if(component.ionViewDidLoad){
+			component.ionViewDidLoad();
+		}
+		this.stopRefresh();
+	  }
 
-  searchMovie(event,key){
-    if(event.target.value.length > 2 ){
-      this.movieService.getMovie(event.target.value).subscribe(
-        data => {
-          this.movies = data.results;
-          console.log(data);
-        },
-        err => {
-          console.log(err);
-        },
-        () => console.log("Done")
-      );
-    }
-  }
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad ListPage');
 
-  getAllMovies(){
-    this.movieService.getMoviesByFilter().subscribe(
-      data => {
-        this.movies = data.results;
-        console.log(data);
-      },
-      err => {
-        console.log(err);
-      },
-      () => console.log("Done")
-    );
-  }
+		this.network.onConnect().subscribe(data => {
+			console.log(data)
+			this.displayNetUpdate(data.type);
+		}, err => console.error(err));
 
-  selectMovie(event, movie) {
+		this.network.onDisconnect().subscribe(data => {
+			console.log(data)
+			this.displayNetUpdate(data.type);
+		}, err => console.error(err));
+
+		this.getAllMovies();
+	}
+
+
+	displayNetUpdate(connectStatus: string){
+		let networkType = this.network.type;
+		this.toast.create({
+			message : `You are now ${connectStatus} via ${networkType}`,
+			duration: 3000
+		}).present();
+	}
+
+	searchMovie(event,key){
+		if(event.target.value.length > 2 ){
+			this.movieService.getMovie(event.target.value).subscribe(
+				data => {
+					this.movies = data.results;
+					console.log(data);
+				},
+				err => {
+					console.log(err);
+				},
+				() => console.log("Done")
+			);
+		}
+	}
+
+	getAllMovies(){
+		this.movieService.getMoviesByFilter().subscribe(
+			data => {
+				this.movies = data.results;
+				console.log(data);
+			},
+			err => {
+				console.log(err);
+			},
+			() => console.log("Done")
+		);
+	}
+
+	selectMovie(event, movie) {
 		console.log(movie+" at home.ts "+event);  
 		this.navCtrl.push(InfoPage, {
 			movie: movie
 		});
-  }
-  
-  presentPopOver(){
-    let popOver = this.popCtrl.create(PopMorePage);
-    popOver.present();
-  }
+	}
+	
+	presentPopOver(){
+		let popOver = this.popCtrl.create(PopMorePage);
+		popOver.present();
+	}
 
-  openAbtUs(){
-    this.navCtrl.push(AboutusPage);
-  }
+	openAbtUs(){
+		this.navCtrl.push(AboutusPage);
+	}
 
-  openCntUs(){
-    this.navCtrl.push(ContactUsPage);
-  }
+	openCntUs(){
+		this.navCtrl.push(ContactUsPage);
+	}
 
 }
